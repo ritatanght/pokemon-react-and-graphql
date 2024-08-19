1030c760ed3b93bbf82de12bac6f6142143b3474
Rita Tang <doggymint@gmail.com>
2024-08-19 16:17:30 -0400

    Set up apollo client and container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { PokemonsContainer } from "./containers/PokemonsContainer";
+
 function App() {
-  return <p>I am a Pokemon</p>;
+  const client = new ApolloClient({
+    uri: "https://graphql-pokemon2.vercel.app/",
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ src/containers/PokemonsContainer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PokemonsContainer = () => {
+  return <p>I am a pokemon container</p>;
+};
